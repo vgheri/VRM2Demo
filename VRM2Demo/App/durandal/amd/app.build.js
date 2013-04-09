d77f47be1c359a3127bbb5ac79eb967abd8c3ca6
Valerio.Gheri <valerio.gheri@gmail.com>
2013-04-09 23:26:08 +0200

    I don't want minification

---
+++ VRM2Demo/App/durandal/amd/app.build.js
@@ -0,0 +1,57 @@
+{
+  "name": "durandal/amd/almond-custom",
+  "inlineText": true,
+  "stubModules": [
+    "durandal/amd/text"
+  ],
+  "paths": {
+    "text": "durandal/amd/text"
+  },
+  "baseUrl": "c:\\Users\\valerio\\Git_Repositories\\VRM2Demo\\VRM2Demo\\App\\durandal\\amd\\../../",
+  "mainConfigFile": "c:\\Users\\valerio\\Git_Repositories\\VRM2Demo\\VRM2Demo\\App\\durandal\\amd\\../../main.js",
+  "include": [
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/main",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/app",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/composition",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/events",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/http",
+    "text!c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/messageBox.html",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/messageBox",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/modalDialog",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/system",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/viewEngine",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/viewLocator",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/viewModel",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/viewModelBinder",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/widget",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/amd/almond-custom",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/amd/app.build",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/amd/r",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/amd/require",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/amd/text",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/plugins/router",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/durandal/transitions/entrance",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/services/logger",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/viewmodels/company",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/viewmodels/details",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/viewmodels/home",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/viewmodels/shell",
+    "c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/viewmodels/vendor",
+    "text!c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/views/footer.html",
+    "text!c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/views/home.html",
+    "text!c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/views/nav.html",
+    "text!c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/views/shell.html",
+    "text!c:/Users/valerio/Git_Repositories/VRM2Demo/VRM2Demo/App/views/vendor.html"
+  ],
+  "exclude": [],
+  "keepBuildDir": true,
+  "optimize": "uglify2",
+  "out": "c:\\Users\\valerio\\Git_Repositories\\VRM2Demo\\VRM2Demo\\App\\durandal\\amd\\../../main-built.js",
+  "pragmas": {
+    "build": true
+  },
+  "wrap": true,
+  "insertRequire": [
+    "main"
+  ]
+}
